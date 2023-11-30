f4e755e094a2a2385adfb1a55631bd0ec381948c
Aaron Maeda <aaronmaeda7311@gmail.com>
2023-12-01 00:00:28 +0300

    Initial Commit

---
+++ asset/Admin_dashboard.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="style2.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <title>HIMS | Dashboard</title>
+    <link rel="icon" type="image/x-icon" href="img\coat.jpg">
+</head>
+
+<body class="body">
+    
+    <!-- sidebar -->
+
+    <section id="sidebar">
+        <a href="#" class="brand nav-link">
+            <i class="bx bxs-smile"></i>
+            <span class="text">Adminhub</span>
+        </a>
+        <ul class="side-menu top list-group">
+            <li class="active">
+            <a href="#" class="nav-link"><i class="bx bxs-dashboard"></i>
+                    <span class="text">Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="nav-link"><i class='bx bxs-analyse'></i>
+                    <span class="text">Analysis</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="nav-link"><i class='bx bxs-doughnut-chart'></i>
+                    <span class="text">Insight</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="nav-link"><i class='bx bxs-analyse'></i>
+                    <span class="text">Analysis</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="nav-link"><i class='bx bxs-doughnut-chart'></i>
+                    <span class="text">Insight</span>
+                </a>
+            </li>
+        </ul>
+        <ul class="side-menu bottom list-group">
+            <li>
+                <a href="#" class="nav-link"><i class="bx bxs-cog"></i>
+                    <span class="text">Settings</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="nav-link text-danger"><i class='bx bx-log-out'></i>
+                    <span class="text">LogOut</span>
+                </a>
+            </li>
+        </ul>
+    </section>
+
+    <!-- sidebar -->
+
+    <!-- content -->
+    <section id="content">
+        <!-- navbar -->
+        <nav>
+            <i class='bx bx-menu' ></i>
+            <a href="#" class="brand nav-link">Categories</a>
+            <form action="#" method="get">
+                <div class="form-input">
+                    <input type="text" placeholder="Search">
+                    <button type="submit" class="search-btn">
+                        <i class="bx bx-search"></i>
+                    </button>
+                </div>
+            </form>
+            <a href="#" class="notification">
+                <i class="bx bx-bell"></i>
+                <span class="num">8</span>
+            </a>
+            <a href="#" class="profile">
+                <img src="img\aroni.jpg" alt="profile pc" srcset="">
+            </a>
+        </nav>
+        <!-- navbar -->
+    </section>
+    <!-- content -->
+
+</body>
+
+<grammarly-desktop-integration data-grammarly-shadow-root="true"></grammarly-desktop-integration>
+<script src="script1.js"></script>
+
+</html>
---
+++ asset/login_page.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--css for login page (custome) -->
+    <link rel="stylesheet" href="login.css">
+     <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script> 
+    <title>Login</title>
+    <link rel="icon" type="image/x-icon" href="img\coat.jpg">
+</head>
+<body>
+    <div class="container">
+        <div class="container-fluid">
+            <div class="bg"></div>
+            <div class="bg bg2"></div>
+            <div class="bg bg3"></div>
+            <div class="content">
+                <div class="position-relative d-flext justify-content-center align-items-cente">
+                <div class="rounded-circle bg-primary" style="width: 200px; height: 200px;"><img src="img\coat.jpg" alt="coat of arm" class="rounded-circle" style="width: 200px; height: 200px;"></div>
+                </div>
+                
+            <form action="" method="post">
+                <div class="form-floating mb-3 mt-3">
+                <label for="Email">Email</label>
+                <input type="email" name="Email" id="" placeholder="Email" class="form-control">
+                </div>
+                <div class="form-floating mb-3">
+                <label for="Email">Password</label>
+                <input type="email" name="Email" id="" placeholder="Email" class="form-control">
+                </div>
+                <div class="justify-content-align align-item-left"><button type="submit" class="btn btn-secondary">Login</button></div>
+            </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
