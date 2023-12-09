a176fea22bc88f00e200e79bc8889734b126b607
Aaron Maeda <aaronmaeda7311@gmail.com>
2023-12-09 03:16:17 +0300

    inntial commit

--- Static/js/script2.js
+++ Static/js/script2.js
@@ -19,9 +19,7 @@ menuBar.addEventListener('click', function () {
 
 
 
-if (window.innerWidth < 768) {
-    sidebar.classList.add('hide');
-}
+
 const searchButton = document.querySelector('#content nav form .form-input button');
 const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
 const searchForm = document.querySelector('#content nav form');
@@ -37,4 +35,19 @@ searchButton.addEventListener('click', function (e) {
         }
     }
 
+});
+
+if (window.innerWidth < 768) {
+    sidebar.classList.add('hide');
+}else if(window.innerWidth > 576){
+    searchButtonIcon.classList.replace('bx-x', 'bx-search');
+    searchForm.classList.remove('show');
+}
+
+window.addEventListener('resize', function(){
+    if(this.innerWidth > 576){
+        searchButtonIcon.classList.replace('bx-x', 'bx-search');
+        searchForm.classList.remove('show');
+    }
+    
 });
